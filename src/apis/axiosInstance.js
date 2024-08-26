0a080723e1086c822062ee38adc99ae73a3da81e
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-08-26 16:19:00 +0700

    move base url to env

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api/v2",
   timeout: 5000,
 });
 
