5ad4e2eacde1676ae2a14e48415ffbc80a2f1b9a
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-08-26 14:20:16 +0700

    create axiosInstance and AuthApi

---
+++ src/apis/AuthApi.js
@@ -0,0 +1,18 @@
+import axiosInstance from "./axiosInstance";
+
+class AuthApi {
+  static async login(username, password) {
+    try {
+      const response = await axiosInstance.post("auth/login", {
+        username,
+        password,
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+export default AuthApi;
