dc221f4606736600044afc21e81b9eed2e7ea770
Syahiid Nur Kamil <syahiidkamil.tech@gmail.com>
2024-08-26 10:04:46 +0700

    simple navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-import "./App.css";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />,
+    children: [
+      {
+        path: "/dashboard",
+        element: (
+          <div>
+            <h1>Dashboard</h1>
+            <Outlet />,
+          </div>
+        ),
+        children: [
+          {
+            path: "",
+            element: <h1>Welcome to Dashboard Home</h1>,
+          },
+          {
+            path: "users",
+            element: <h1>User List</h1>,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <h1>Login</h1>,
+  },
+]);
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
